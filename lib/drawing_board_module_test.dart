5df4cc40022f8b9bf154390033b295bd2c5af836
kimseongsun <kimseongson@gmail.com>
2024-11-26 13:19:19 +0900

    수정된 코드와 다빈형님이 업데이트한 draw_moudle_test 연결

--- lib/drawing_board_module_test.dart
+++ lib/drawing_board_module_test.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Firebase에서 실시간으로 이미지 데이터를 받아와서 업데이트합니다.
 class DrawingPage extends StatefulWidget {
   const DrawingPage({super.key});
 
@@ -35,58 +36,12 @@ class DrawingPage extends StatefulWidget {
   State<DrawingPage> createState() => _DrawingPageState();
 }
 
-class _DrawingPageState extends State<DrawingPage> with SingleTickerProviderStateMixin {
+class _DrawingPageState extends State<DrawingPage> {
   final DrawingController _drawingController = DrawingController();
-  final String promptWord = "애 호 박";
-  late AnimationController _timerController;
-  final TransformationController _transformationController = TransformationController();
-  Color timeColor = Colors.green;
-  final double first_timeWidth = 300.0;
-  double timeWidth = 300.0;
+  final String promptWord = "애 호 박"; // 예시 단어
   bool isTimeLow = false;
 
-  double _colorOpacity = 1;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  @override
-  void initState() {
-    super.initState();
-    _timerController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 10), // Set the desired countdown time
-    )
-      ..addListener(() {
-        setState(() {
-          // Update time bar color and width based on remaining time
-          double progress = _timerController.value;
-          timeColor = Color.lerp(Colors.green, Colors.red, progress)!;
-          timeWidth = 300 * (1 - progress);
-
-          // Trigger shake effect when time is low
-          if (progress > 0.8) {
-            isTimeLow = true;
-          }
-        });
-      });
-
-    // Start the timer when the game starts
-    _timerController.forward();
-
-    Timer? _timer;
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      _getImageData();
-    });
-    //1초마다 이미지 업로드
-  }
-
-
-
-  @override
-  void dispose() {
-    _timerController.dispose();
-    _drawingController.dispose();
-    super.dispose();
-  }
+  // Firebase Database에 이미지를 업로드하는 메소드
   Future<void> _uploadImage(Uint8List imageData) async {
     String base64String = base64Encode(imageData);
 
@@ -118,45 +73,46 @@ class _DrawingPageState extends State<DrawingPage> with SingleTickerProviderStat
     print("Uploaded Image and cleaned up old entries.");
   }
 
-
-  /// Capture the drawing data as image and upload
+  // 그림 데이터를 주기적으로 Firebase에 업로드
   Future<void> _getImageData() async {
     final Uint8List? data = (await _drawingController.getImageData())?.buffer.asUint8List();
     if (data == null) {
-
       debugPrint('Failed to get image data');
       return;
     }
-    // Upload the image to Firebase
-    //await testUploadImage();
 
-
+    // Firebase에 업로드
     await _uploadImage(data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 일정 시간마다 이미지 데이터를 업로드합니다.
+    Timer.periodic(Duration(microseconds: 700), (timer) {
+      _getImageData();
+      //yum
+    });
+  }
+
+  @override
+  void dispose() {
+    _drawingController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.grey,
       body: Stack(
         children: <Widget>[
-          // Main content
           Column(
             children: <Widget>[
-              // Prompt Word
               SizedBox(height: 25),
               Text(
                 promptWord,
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Transform.translate(
-                offset: isTimeLow ? Offset(5 * (0.5 - _timerController.value), 0) : Offset(0, 0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width * (timeWidth / first_timeWidth),
-                  height: 3,
-                  color: timeColor,
-                ),
               ),
               SizedBox(height: 10),
               Expanded(
@@ -165,7 +121,6 @@ class _DrawingPageState extends State<DrawingPage> with SingleTickerProviderStat
                     return DrawingBoard(
                       boardPanEnabled: false,
                       boardScaleEnabled: false,
-                      transformationController: _transformationController,
                       controller: _drawingController,
                       background: Container(
                         width: constraints.maxWidth,
@@ -175,11 +130,9 @@ class _DrawingPageState extends State<DrawingPage> with SingleTickerProviderStat
                       showDefaultActions: true,
                       showDefaultTools: true,
                     );
-                    //DRAWING BOARD
                   },
                 ),
               ),
-
             ],
           ),
 
